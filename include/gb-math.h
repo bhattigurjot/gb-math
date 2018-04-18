79f5d4d507c97711f31fda093beea14acfcd246b
bhattigurjot <bhattigurjot@gmail.com>
2018-04-17 20:25:31 -0600

    Moved function definitions inside the class template

--- include/gb-math.h
+++ include/gb-math.h
@@ -15,9 +15,9 @@ public:
 		y(static_cast<T>(0))
 	{}
 
-	// Only float values for now
-	explicit Vector2(T _x, T _y)
-		:x(_x),
+	// Constructor with values
+	explicit Vector2(T _x, T _y) :
+		x(_x),
 		y(_y)
 	{}
 
@@ -28,23 +28,52 @@ public:
 	// Set y value
 	void SetY(T _y) { y = _y; }
 
-	Vector2<T> operator+(const Vector2<T>&);
-	Vector2<T>& operator+=(const Vector2<T>&);
-	Vector2<T> operator-(const Vector2<T>&);
-	Vector2<T>& operator-=(const Vector2<T>&);
-	Vector2<T> operator*(const T);
-	Vector2<T>& operator*=(const T);
-	Vector2<T> operator/(const T);
-	Vector2<T>& operator/=(const T);
-
 	// Static functions
 	static const Vector2<T> Zero;
 	static const Vector2<T> Unit;
-};
 
-typedef Vector2<int> Vector2i;
-typedef Vector2<float> Vector2f;
-typedef Vector2<double> Vector2d;
+	// Vector Mathematics
+	Vector2<T> operator+(const Vector2<T>& _vec) {
+		// v = a + b
+		return Vector2<T>(x + _vec.x, y + _vec.y);
+	}
+	Vector2<T>& operator+=(const Vector2<T>& _vec) {
+		// v += a
+		x += _vec.x;
+		y += _vec.y;
+		return *this;
+	}
+	Vector2<T> operator-(const Vector2<T>& _vec) {
+		// v = a - b
+		return Vector2<T>(x - _vec.x, y - _vec.y);
+	}
+	Vector2<T>& operator-=(const Vector2<T>& _vec) {
+		// v -= a
+		x -= _vec.x;
+		y -= _vec.y;
+		return *this;
+	}
+	Vector2<T> operator*(const T _val) {
+		// v = a * scaler
+		return Vector2<T>(x * _val, y * _val);
+	}
+	Vector2<T>& operator*=(const T _val) {
+		// v *= scaler
+		x *= _val;
+		y *= _val;
+		return *this;
+	}
+	Vector2<T> operator/(const T _val) {
+		// v = a / scaler
+		return Vector2<T>(x / _val, y / _val);
+	}
+	Vector2<T>& operator/=(const T _val) {
+		// v /= scaler
+		x /= _val;
+		y /= _val;
+		return *this;
+	}
+};
 
 template<class T>
 const Vector2<T> Vector2<T>::Zero(static_cast<T>(0), static_cast<T>(0));
@@ -52,66 +81,6 @@ const Vector2<T> Vector2<T>::Zero(static_cast<T>(0), static_cast<T>(0));
 template<class T>
 const Vector2<T> Vector2<T>::Unit(static_cast<T>(1), static_cast<T>(1));
 
-template<class T>
-Vector2<T> Vector2<T>::operator+(const Vector2<T>& _vec)
-{
-	return Vector2<T>(x + _vec.x,
-		y + _vec.y);
-}
-
-template<class T>
-Vector2<T>& Vector2<T>::operator+=(const Vector2<T>& _vec)
-{
-	x += _vec.x;
-	y += _vec.y;
-
-	return *this;
-}
-
-template<class T>
-Vector2<T> Vector2<T>::operator-(const Vector2<T>& _vec)
-{
-	return Vector2<T>(x - _vec.x,
-		y - _vec.y);
-}
-
-template<class T>
-Vector2<T>& Vector2<T>::operator-=(const Vector2<T>& _vec)
-{
-	x -= _vec.x;
-	y -= _vec.y;
-
-	return *this;
-}
-
-template<class T>
-Vector2<T> Vector2<T>::operator*(const T _val)
-{
-	return Vector2<T>(x * _val,
-		y * _val);
-}
-
-template<class T>
-Vector2<T>& Vector2<T>::operator*=(const T _val)
-{
-	x *= _val;
-	y *= _val;
-
-	return *this;
-}
-
-template<class T>
-Vector2<T> Vector2<T>::operator/(const T _val)
-{
-	return Vector2<T>(x / _val,
-		y / _val);
-}
-
-template<class T>
-Vector2<T>& Vector2<T>::operator/=(const T _val)
-{
-	x /= _val;
-	y /= _val;
-
-	return *this;
-}
+typedef Vector2<int> Vector2i;
+typedef Vector2<float> Vector2f;
+typedef Vector2<double> Vector2d;
